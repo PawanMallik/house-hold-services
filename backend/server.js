18b3602b88518c3a3ca2637544da5da2e93d9fdb
Pawan <pawanmallick432003@gmail.com>
2025-08-17 16:59:24 +0530

    fix ishu

--- backend/server.js
+++ backend/server.js
@@ -22,9 +22,10 @@ const db = mysql.createConnection({
   database: process.env.DB_NAME,
   port: process.env.DB_PORT || 3306,
   ssl: {
-    rejectUnauthorized: true,
+    rejectUnauthorized: false, // ✅ Fix for Railway MySQL
   },
 });
+
 
 db.connect((err) => {
   if (err) {
