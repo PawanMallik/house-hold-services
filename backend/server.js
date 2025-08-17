4e26185bd8069eee4cb4641b788f57b66f218aef
Pawan <pawanmallick432003@gmail.com>
2025-08-17 17:28:16 +0530

    test

--- backend/server.js
+++ backend/server.js
@@ -15,16 +15,15 @@ app.use(cors({ origin: corsOrigin, credentials: true }));
 app.use(express.json());
 
 // Database connection (Railway MySQL with SSL)
-const pool = mysql.createPool({
+const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT || 3306,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+
 });
+
 
 db.connect((err) => {
   if (err) {
@@ -148,4 +147,3 @@ app.listen(PORT, () => {
 });
 
 module.exports = app;
-module.exports = pool;
