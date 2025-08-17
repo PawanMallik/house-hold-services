047fcd93e6344b9f5e4dbe71f421d3c89b20d168
Pawan <pawanmallick432003@gmail.com>
2025-08-17 16:20:26 +0530

    remove gitignore file

---
+++ backend/test.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql2');
+require('dotenv').config();
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: { rejectUnauthorized: true }, // only if Railway requires SSL
+});
+
+db.connect(err => {
+  if (err) console.error('Connection failed:', err);
+  else console.log('Connected to Railway MySQL!');
+});
